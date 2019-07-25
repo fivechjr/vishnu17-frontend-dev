366673f7ea09403cd9f2681a939ecf071eddec6e
Patcharapat Chaijaroen <f5.chjr@gmail.com>
2019-07-25 17:40:27 +0700

    Revert: 25483e94c26d60a7d0b075dd6e95a50a2915272e

--- src/components/statuses/list-item.vue
+++ src/components/statuses/list-item.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-sm w-full flex flex-row items-center border-b" style="min-width: 768px">
-        <div class="w-3/6 flex flex-row items-center px-4 py-3">
+        <div class="w-2/6 flex flex-row items-center px-4 py-3">
             <div class="w-8 flex-shrink-0 flex mr-4">
                 <p class="text-sm text-gray-4">{{computedIndex}}</p>
             </div>
@@ -21,7 +21,7 @@
                 >{{data.person.student_id}} &mdash; ชั้นปีที่ {{data.person.year}}</span>
             </div>
         </div>
-        <div class="w-1/6 flex flex-col px-4 py-3">
+        <div class="w-2/6 flex flex-col px-4 py-3">
             <span
                 class="text-xs uppercase tracking-wide"
                 :class="isPresent ? 'text-teal-500' : (isUnexpected ? 'text-red-500': 'text-gray-4')"
